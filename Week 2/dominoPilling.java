bfec394b386447700662d02ba537e8dd4917f0b6
nahom-getachew7gi5 <ets050012feng@aastu.edu.et>
2022-04-24 12:28:51 +0300

    submit answer for domino problem

---
+++ Week 2/dominoPilling.java
@@ -0,0 +1,15 @@
+package learn;
+
+import java.util.Scanner;
+
+public class dominoPilling {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		int M= input.nextInt();
+		int N= input.nextInt();
+		System.out.println((M*N)/2);
+		
+	}
+
+}
