0adbbf75ff5917c97a26b8314f45426f2bbc83ab
Nahom Getachew <88863094+nahom-getachew7@users.noreply.github.com>
2023-04-04 23:56:08 +0300

    Update HowManyNo.py

--- HowManyNo.py
+++ HowManyNo.py
@@ -1,11 +1,17 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        mar = []
-        z = 0
-        for x in nums:
-            for y in nums:
-                if x > y:
-                    z+=1
-            mar.append(z)
-            z =0
-        return mar
+        mar = [0] * len(nums)
+        z =0
+        for i in range (len(nums)):
+            nums[i] = [i,nums[i]]
+        nums.sort(key =lambda x:x[1])
+        prievious = nums[0][1]
+        for i in range (1,len(nums)):
+            if prievious == nums[i][1]:
+                mar[nums[i][0]] = z
+            else:
+                z = i
+                mar[nums[i][0]] = z
+            prievious = nums[i][1]
+
+        return (mar)
